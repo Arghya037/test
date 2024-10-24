49ddd84b984ffe5c9c3dd6c6d55cdd25ca8e8dd5
Arghya037 <arghyamitra114@gmail.com>
2024-10-24 15:53:18 +0530

    test

---
+++ tutor finder/singup.js
@@ -0,0 +1,24 @@
+
+
+const name = document.getElementById("name");
+
+const email = document.getElementById("email");
+
+const number = document.getElementById("number");
+const age = document.getElementById("age");
+const city = document.getElementById("city");
+const pincode = document.getElementById("pincode");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+const siform = document.getElementById("siform");
+
+
+
+siform.addEventListener('submit',(e)=>
+{
+   
+
+
+
+
+})
